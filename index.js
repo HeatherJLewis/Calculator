7638b204e6572c7a0c77e0a10c5b2e2a1ebb880a
Heather Lewis <heather.lewis@bbc.co.uk>
2020-06-27 12:14:38 +0100

    Adds a simple multiplier

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('Welcome to the calculator!');
+const readline = require('readline-sync');
+
+console.log('Please enter a number:');
+
+const input1 = parseInt(readline.prompt());
+
+console.log('Please enter another number:');
+
+const input2 = parseInt(readline.prompt());
+
+const multiplier = (x, y) => {
+    return x * y
+}
+
+console.log(multiplier(input1, input2));
