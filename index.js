203a087d95faca09039b5f5d399b8623c8190dae
Heather Lewis <heather.lewis@bbc.co.uk>
2020-06-28 10:32:52 +0100

    Adds subtract, divide and add operators

--- index.js
+++ index.js
@@ -1,15 +1,78 @@
 const readline = require('readline-sync');
 
-console.log('Please enter a number:');
+let input1;
+let input2;
+let answer;
 
-const input1 = parseInt(readline.prompt());
+console.log('Welcome to the calculator!');
 
-console.log('Please enter another number:');
+console.log('Please enter an operator');
 
-const input2 = parseInt(readline.prompt());
+const operator = readline.prompt();
+
+const logOperator = () => console.log(`You have chosen ${operator}`);
+
+const askForInput1 = () => {
+    console.log('Please enter the first number:');
+
+    return input1 = parseInt(readline.prompt());
+};
+
+const askForInput2 = () => {
+    console.log('Please enter the second number:');
+
+    return input2 = parseInt(readline.prompt());
+};
 
 const multiplier = (x, y) => {
     return x * y
+};
+
+const adder = (x, y) => {
+    return x + y
+};
+
+const subtracter = (x, y) => {
+    return x - y
+};
+
+const divider = (x, y) => {
+    return x / y
+};
+
+switch (operator) {
+    case "*":
+        logOperator()
+        askForInput1();
+        askForInput2();
+        answer = multiplier(input1, input2);
+        break;
+
+    case "+":
+        logOperator()
+        askForInput1();
+        askForInput2();
+        answer = adder(input1, input2);
+        break;
+
+    case "-":
+        logOperator()
+        askForInput1();
+        askForInput2();
+        answer = subtracter(input1, input2);
+        break;
+
+    case "/":
+        logOperator()
+        askForInput1();
+        askForInput2();
+        answer = divider(input1, input2);
+        break;
+
+    default:
+        console.log('We havent made that function yet!');
+        break;
 }
 
-console.log(multiplier(input1, input2));
+
+console.log((!answer) ? 'Try again please' : `The answer is: ${answer}`)
