1042f269fdc716c75b1af13d400e645dc66494af
Heather Lewis <heather.lewis@bbc.co.uk>
2020-06-28 11:52:36 +0100

    Refactors to use an array for the operands

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const readline = require('readline-sync');
 
 let answer = 0;
+let operandsArray = [];
 
+const createOperandsArray = number => {
+    for (let i = 1; i <= number; i++) {
+        console.log(`Please enter number ${i}`);
+        operandsArray.push(parseInt(readline.prompt()))
+    }
+    return operandsArray;
+}
 console.log('Welcome to the calculator!');
 
 console.log('Please enter an operator');
@@ -12,30 +20,26 @@ console.log(`You have chosen ${operator} \nHow many numbers would you like?`);
 
 const numberOfOperands = +readline.prompt();
 
-const adder = number => {
-    for(let i = 1; i <= number; i++) {
-        console.log(`Please enter number ${i}`);
-        answer += (parseInt(readline.prompt()));
-    }
+operandsArray = createOperandsArray(numberOfOperands);
+
+const adder = () => {
+    operandsArray.forEach(el => answer += el)
     return answer;
 };
 
-const multiplier = number => {
+const multiplier = () => {
     answer = 1;
-    for(let i = 1; i <= number; i++) {
-        console.log(`Please enter number ${i}`);
-        answer *= (parseInt(readline.prompt()));
-    }
+    operandsArray.forEach(el => answer *= el)
     return answer;
 };
 
 switch (operator) {
     case "*":
-        multiplier(numberOfOperands)
+        multiplier()
         break;
 
     case "+":
-        adder(numberOfOperands)
+        adder()
         break;
 
     case "-":
